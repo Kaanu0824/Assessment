6675f94c6b9bba6cc8782377df5c58df9e18145b
Kaanu0824 <it20616274@my.sliit.lk>
2023-03-08 15:26:32 +0530

    DB Connection and Create Node

---
+++ app.js
@@ -0,0 +1,34 @@
+const mongoose=require("mongoose")
+const express = require("express")
+const app = express()
+
+
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require("dotenv").config();
+
+
+
+//DB Connection
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    console.log("DB Connected")
+}).catch(()=>{
+    console.log("Unable to connect DB")
+})
+
+//use parsing middlwware
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+ 
+const port = process.env.PORT || 8000
+
+//Starting a server
+app.listen(port, () => {
+    console.log(`App is running at ${port}`)
+  })
